8ecb7af3b1f88c65515c28ac24aa5334df109bec
Bill <wkernan@gmail.com>
2016-11-30 10:32:19 -0600

    multiplayer movement using onkeyup/onkeydown

--- app.js
+++ app.js
@@ -13,12 +13,61 @@ serv.listen(app.get('port'), () => {
 });
 
 const SOCKET_LIST = {};
+const PLAYER_LIST = {};
+
+const Player = id => {
+	const self = {
+		x: 250,
+		y:250,
+		id,
+		number: `${Math.floor(10 * Math.random())}`,
+		pressingLeft: false,
+		pressingRight: false,
+		pressingUp: false,
+		pressingDown: false,
+		maxSpd: 10
+	}
+
+	self.updatePosition = () => {
+		if(self.pressingRight) 
+			self.x += self.maxSpd;
+		if(self.pressingLeft) 
+			self.x -= self.maxSpd;
+		if(self.pressingUp)
+			self.y -= self.maxSpd;
+		if(self.pressingDown)
+			self.y += self.maxSpd;
+	}
+
+	return self;
+};
+
 const io = require('socket.io')(serv, {});
 io.sockets.on('connection', socket => {
 	socket.id = Math.random();
 	socket.x = 0;
 	socket.y = 0;
+	socket.number = "" + Math.floor(10 * Math.random());
 	SOCKET_LIST[socket.id] = socket;
+	
+	const player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
+
+	socket.on('disconnect', () => {
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+	});
+
+	socket.on('keyPress', data => {
+		if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		else if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		else if(data.inputId === 'down')
+			player.pressingDown = data.state;
+		else if(data.inputId === 'up')
+			player.pressingUp = data.state;
+	})
 
 	socket.emit('serverMsg', {
 		msg: `Player ${socket.id} has joined!`
@@ -27,13 +76,13 @@ io.sockets.on('connection', socket => {
 
 setInterval(() => {
 	const pack = [];
-	for(const i in SOCKET_LIST) {
-		const socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;
+	for(const i in PLAYER_LIST) {
+		const player = PLAYER_LIST[i];
+		player.updatePosition();
 		pack.push({
-			x: socket.x,
-			y: socket.y
+			x: player.x,
+			y: player.y,
+			number: player.number
 		});
 	};
 
